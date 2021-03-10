1e02f90c0729f90978d620d8a2048a506c06c12e
unknown <pablojpba@gmail.com>
2021-03-10 10:57:39 -0300

    que onda este settin

--- pelicanconf.py
+++ pelicanconf.py
@@ -35,7 +35,7 @@ LINKS = (('Pelican', 'https://getpelican.com/'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 '''
 jupyter - pelican
