3aed9d5c22b3de69807198ab8413fa38d1c47546
unknown <pablojpba@gmail.com>
2021-03-19 12:27:15 -0300

    yo yo what up bro

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-# SITEURL = 'https://pyblobenavides.github.io/pablobenavides.site'
-SITEURL = 'http://pablobenavides.site'
+SITEURL = 'https://pyblobenavides.github.io/pablobenavides.site'
+# SITEURL = 'http://pablobenavides.site'
 RELATIVE_URLS = True
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
